6ce987b0d02e944672c2ebd3d66e1422ac8f10d4
Ian Wijma <ian@wij.ma>
2023-10-28 17:04:57 +1100

    Added an export for AbstractAttributeHandler

--- src/index.mjs
+++ src/index.mjs
@@ -1,2 +1,3 @@
 export { startVjs } from './startVjs.mjs'
+export { AbstractAttributeHandler } from './attributeHandlers/abstractAttributeHandler.mjs'
 export * as attributeHandlers from './attributeHandlers';
